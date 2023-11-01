44e443db14bd4ba77570a88dd3a829fa0cc8bb4f
Ian Ross <ianrossdrums@gmail.com>
2023-11-01 12:55:11 -0600

    Add end game grid

--- input.js
+++ input.js
@@ -1,23 +1,30 @@
-let inputDirecetion = { x: 0, y: 0};
+let inputDirecetion = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 }
 
 window.addEventListener('keydown', e => {
 	switch (e.key) {
 		case 'ArrowUp':
+			// Snake can't go up if already going down
+			if (lastInputDirection.y !== 0) break
 			inputDirecetion = { x: 0, y: -1 };
 			break;
 		case 'ArrowDown':
+			if (lastInputDirection.y !== 0) break
 			inputDirecetion = { x: 0, y: 1 };
 			break;
 		case 'ArrowLeft':
+			if (lastInputDirection.x !== 0) break
 			inputDirecetion = { x: -1, y: 0 };
 			break;
 		case 'ArrowRight':
+			if (lastInputDirection.x !== 0) break
 			inputDirecetion = { x: 1, y: 0 };
 			break;
 	}
 })
 
 export function getInputDirection() {
+	lastInputDirection = inputDirecetion;
   return inputDirecetion;
 }
 
